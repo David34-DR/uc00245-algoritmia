34ac2514f6ef25236c8ce61def152a70e1744a10
David34-DR <david.rolo.2205@gmail.com>
2025-10-29 15:48:26 +0000

    Alteraçao de Ficheiros em 0245

--- 00245/src/app/app.ts
+++ 00245/src/app/app.ts
@@ -1,15 +1,15 @@
-import { Component} from '@angular/core';
-import { Header } from './components/header/header';
-import { Main } from './components/main/main';
-import { Footer } from "./components/footer/footer";
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./componentes/header/header";
+import { Main } from "./componentes/main/main";
+import { Footer } from "./componentes/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Main, Footer],
+  imports: [RouterOutlet, Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  //protected readonly title = signal('00245');
-
+// protected readonly title = signal('00245');
 }
